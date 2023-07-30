eedb5e602fbe634716081d2ba98fb4680f9e0a59
fartun <fartunwario34@gmail.com>
2023-07-30 11:13:04 +0300

    update userprofile

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserProfile from './components/UserProfile'; 
+
+function App() {
+  return (
+    <div>
+      <h1>UserProfile</h1>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
