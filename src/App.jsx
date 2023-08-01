323dd6a9a227cbb87b8bf32c8ceef60408589297
fartun <fartunwario34@gmail.com>
2023-08-01 10:27:53 +0300

    UserProfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import UserProfile from './components/UserProfile'; 
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
-      <h1>UserProfile</h1>
       <UserProfile />
     </div>
   );
