9140b79be0118cc0e8dbde3da1a7038b63cf5079
fartun <fartunwario34@gmail.com>
2023-08-09 20:55:08 +0300

    add notification page

--- src/components/NotificationPage.js
+++ src/components/NotificationPage.js
@@ -1,13 +1,9 @@
-// NotificationPage.jsx
 import React from 'react';
-import './NotificationPage.css';
 const NotificationPage = () => {
   return (
     <div className="notification-container">
       <h1>Notification</h1>
       <div className="notification">
-        <img src="accept-icon.png" alt="Accept Icon" />
-        <p>Your cleaning request has been accepted by the cleaner.</p>
       </div>
     </div>
   );
